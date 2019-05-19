b5ff3177c91968ccee63dddc7ac923b1dd704ba1
Matt Condor <mcondor@mercurynewmedia.com>
2019-05-19 19:06:29 -0400

    Add map search for arc gis map and location inforamtion on click

---
+++ FloodProject/FloodProject/wwwroot/js/arc-gis.js
@@ -0,0 +1,60 @@
+﻿//This fires when the document loads. 
+$(document).ready(function () {
+    console.log("ready!");
+
+    Create_Map(); 
+});
+
+Create_Map = () => {
+    require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/widgets/Search" //Adds the map search function to ArgGIS
+    ], function (Map, MapView, Search) {
+
+        var map = new Map({
+            basemap: "topo-vector"
+        });
+
+        var view = new MapView({
+            container: "GIS-Map",
+            map: map,
+            center: [-82.7344711, 27.9941986], //This is the lat/long for Tampa
+            zoom: 10
+        });
+
+        // Search widget
+        var search = new Search({
+            view: view
+        });
+
+        view.ui.add(search, "top-right");
+
+        Setup_Click_To_Search(view, search); 
+    });
+}; 
+
+Setup_Click_To_Search = (view, search) => {
+    view.on("click", function (evt) {
+        search.clear();
+        view.popup.clear();
+        if (search.activeSource) {
+            var geocoder = search.activeSource.locator; // World geocode service
+            geocoder.locationToAddress(evt.mapPoint)
+                .then(function (response) { // Show the address found
+                    var address = response.address;
+                    showPopup(address, evt.mapPoint);
+                }, function (err) { // Show no address found
+                    showPopup("No address found.", evt.mapPoint);
+                });
+        }
+    });
+
+    function showPopup(address, pt) {
+        view.popup.open({
+            title: + Math.round(pt.longitude * 100000) / 100000 + "," + Math.round(pt.latitude * 100000) / 100000,
+            content: address,
+            location: pt
+        });
+    }
+}
