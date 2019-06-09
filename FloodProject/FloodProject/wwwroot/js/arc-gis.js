8272fbcf6c3afdc5b938e01e13d4be44b147ddf6
Sean King <SeanNole90@outlook.com>
2019-06-08 22:57:15 -0400

    code changes to on-click and search popup

--- FloodProject/FloodProject/wwwroot/js/arc-gis.js
+++ FloodProject/FloodProject/wwwroot/js/arc-gis.js
@@ -63,12 +63,12 @@ Create_Map = () => {
                 success: function (response) {
                     search.popupTemplate.title = "<h2>Location Results: </h2>",
                     search.popupTemplate.content =
-                    "<h5>Coordinates:</h5>" + Math.round(mapCenter.longitude * 100000) / 100000 + ", " + Math.round(mapCenter.latitude * 100000) / 100000 
-                    + "<h5>Address:</h5>" + event.searchTerm + "<br>"
-                    + "<h5>Flood Zone:</h5>Zone " + response.data.floodZone 
-                    + "<h5>Catastrophic Flood Probability:</h5>" + response.data.floodZoneDesciption 
-                    + "<h5>Base Flood Elevation:</h5>" + null //placeholder for BFE
-                    + "<h5>Flood Insurance Required?</h5>" + response.data.SpecialFloodHazardArea; 
+                    "<b>Coordinates: </b>" + Math.round(mapCenter.longitude * 100000) / 100000 + ", " + Math.round(mapCenter.latitude * 100000) / 100000 + "<br><br>"
+                    + "<b>Address: </b>" + event.searchTerm + "<br><br>"
+                    + "<b>Flood Zone:</b> Zone " + response.data.floodZone + "<br><br>"
+                    + "<b>Catastrophic Flood Probability: </b>" + response.data.floodZoneDesciption + "<br><br>"
+                    + "<b>Base Flood Elevation: </b>" + null + "<br><br>"//placeholder for BFE
+                    + "<b>Flood Insurance Required? </b>" + response.data.SpecialFloodHazardArea; // SFA not displaying correctly, "undefined"
                 },
                 error: function () {
                     // Hope this works
@@ -122,8 +122,14 @@ Setup_Click_To_Search = (view, search) => {
             },
             success: function (response) {
                 view.popup.open({
-                    title: "Coords: " + Math.round(pt.longitude * 100000) / 100000 + "," + Math.round(pt.latitude * 100000) / 100000,
-                    content: address + "<h5>" + response.data.floodZoneDesciption + "</h5>"
+                    title: "<h2>Location Results</h2>",
+                    content:
+                        "<b>Coordinates: </b>" + Math.round(pt.longitude * 100000) / 100000 + ", " + Math.round(pt.latitude * 100000) / 100000 + "<br><br>"
+                        + "<b>Address: </b>" + address + "<br><br>"
+                        + "<b>Flood Zone: </b> Zone " + response.data.floodZone + "<br><br>"
+                        + "<b>Catasrophic Flood Probability: </b>" + response.data.floodZoneDesciption + "<br><br>"
+                        + "<b>Base Flood Elevation: </b>" + null + "<br><br>" //placeholder for BFE
+                        + "<b>Flood Insurance Required? </b>" + response.data.SpecialFloodHazardArea //SFA data not displaying correctly, "undefined"
                 });
             },
             error: function () {
