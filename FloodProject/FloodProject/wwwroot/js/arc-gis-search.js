bdb5a556c06ff7dcaf547f067acead1296686b6a
Michael Lee <mlee@mail.usf.edu>
2019-06-08 23:32:06 -0400

    General js code clean up - created seperate js file for custom search responses

---
+++ FloodProject/FloodProject/wwwroot/js/arc-gis-search.js
@@ -0,0 +1,63 @@
+﻿Setup_Custom_Search_Response = (view, search) => {
+    // Click to Search
+    view.on("click", function (evt) {
+        search.clear();
+        view.popup.clear();
+        if (search.activeSource) {
+            var geocoder = search.activeSource.locator; // World geocode service
+            geocoder.locationToAddress(evt.mapPoint)
+                .then(function (response) {
+                    var address = response.address;
+                    showPopup(address, evt.mapPoint);
+                }, function (err) { // Show no address found
+                    showPopup("No address found.", evt.mapPoint);
+                });
+        }
+    });
+
+    // Manual Search
+    search.on("search-complete", function (event) {
+        var coords = event.results[0].results[0].feature.geometry;
+        if (search.activeSource) {
+            var geocoder = search.activeSource.locator; // World geocode service
+            geocoder.locationToAddress(coords)
+                .then(function (response) {
+                    var address = response.address;
+                    showPopup(address, coords);
+                }, function (err) { // Show no address found
+                    showPopup("No address found.", coords);
+                });
+        }
+    });
+
+    /// Pass/receive data from HomeController using ajax
+    /// and updates the popup content
+    function showPopup(address, coords) {
+        $.ajax({
+            url: 'Home/GetFloodDataByCoordinates',
+            type: 'GET',
+            dataType: 'json',
+            traditional: true,
+            data: {
+                latitude: coords.latitude,
+                longitude: coords.longitude
+            },
+            success: function (response) {
+                view.popup.open({
+                    title: "<h2>Location Results: </h2>",
+                    content: "<b>Coordinates: </b>" + Math.round(coords.longitude * 100000) / 100000 + ", "
+                        + Math.round(coords.latitude * 100000) / 100000 + "<br><br>"
+                        + "<b>Address: </b>" + address + "<br><br>"
+                        + "<b>Flood Zone:</b> Zone " + response.data.floodZone + "<br><br>"
+                        + "<b>Catastrophic Flood Probability: </b>" + response.data.floodZoneDesciption + "<br><br>"
+                        + "<b>Base Flood Elevation: </b>" + null + "<br><br>"//placeholder for BFE
+                        + "<b>Flood Insurance Required? </b>" + response.data.specialFloodHazardArea
+                });
+            },
+            error: function () {
+                //Swallow it, who cares lolz
+            }
+        });
+    }
+
+};
--- FloodProject/FloodProject/wwwroot/js/arc-gis.js
+++ FloodProject/FloodProject/wwwroot/js/arc-gis.js
@@ -24,11 +24,9 @@ Create_Map = () => {
             zoom: 15 //This zoom level shows the flood layers and legend.  Higher numbers = more zoomed in. 
         });
 
-        // Adds feature layer to basemap
-        var layer = new MapLayer({
+        var layer = new MapLayer({ // Adds in FEMA FIRM map layer
             url: "https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer",
-            /// Limit sublayers displayed to those listed
-            /// Ideally what layers to show will be togglable in the future
+
             sublayers: [
               {
                 id: 28, // Flood Hazard Zones layer
@@ -41,101 +39,20 @@ Create_Map = () => {
             ]
         });
 
-
-        // Search widget
-        var search = new Search({
+        var search = new Search({ // Creates search widget
             view: view,
         });
-        /// On search complete pass/receive data from HomeController
-        /// using ajax and update the search popup content
-        search.on("search-complete", function (event) {
-            /// Map center's on the popup/result after searching
-            /// Using this to grab coords based on map center
-            var mapCenter = view.center.clone();
-            $.ajax({
-                url: 'Home/GetFloodDataByCoordinates',
-                type: 'GET',
-                traditional: true,
-                data: {
-                    latitude: mapCenter.latitude,
-                    longitude: mapCenter.longitude
-                },
-                success: function (response) {
-                    search.popupTemplate.title = "<h2>Location Results: </h2>",
-                    search.popupTemplate.content =
-                    "<b>Coordinates: </b>" + Math.round(mapCenter.longitude * 100000) / 100000 + ", " + Math.round(mapCenter.latitude * 100000) / 100000 + "<br><br>"
-                    + "<b>Address: </b>" + event.searchTerm + "<br><br>"
-                    + "<b>Flood Zone:</b> Zone " + response.data.floodZone + "<br><br>"
-                    + "<b>Catastrophic Flood Probability: </b>" + response.data.floodZoneDesciption + "<br><br>"
-                    + "<b>Base Flood Elevation: </b>" + null + "<br><br>"//placeholder for BFE
-                    + "<b>Flood Insurance Required? </b>" + response.data.SpecialFloodHazardArea; // SFA not displaying correctly, "undefined"
-                },
-                error: function () {
-                    // Hope this works
-                }
-            });
-        });
 
-        // Legend widget
-        var legend = new Legend({
+        var legend = new Legend({ // Creates legend widget
             view: view
         });
 
-        // Adds the layer defined abocve as an overlay on the basemap
-        map.add(layer);
-        // Displays the search widget created above
-        view.ui.add(search, "top-right");
-        // Displays the legend widget created above
-        view.ui.add(legend, "bottom-left");
-        // Fires click to search 
-        Setup_Click_To_Search(view, search);
-    });
-}; 
-
-Setup_Click_To_Search = (view, search) => {
-    view.on("click", function (evt) {
-        search.clear();
-        view.popup.clear();
-        if (search.activeSource) {
-            var geocoder = search.activeSource.locator; // World geocode service
-            geocoder.locationToAddress(evt.mapPoint)
-                .then(function (response) {
-                    var address = response.address;
-                    showPopup(address, evt.mapPoint);
-                }, function (err) { // Show no address found
-                    showPopup("No address found.", evt.mapPoint);
-                });
-        }
-    });
-
-    /// Pass/receive data from HomeController using ajax
-    /// and update the on-click popup content
-    function showPopup(address, pt) {
-        $.ajax({
-            url: 'Home/GetFloodDataByCoordinates',
-            type: 'GET',
-            dataType: 'json',
-            traditional: true,
-            data: {
-                latitude: pt.latitude,
-                longitude: pt.longitude
-            },
-            success: function (response) {
-                view.popup.open({
-                    title: "<h2>Location Results</h2>",
-                    content:
-                        "<b>Coordinates: </b>" + Math.round(pt.longitude * 100000) / 100000 + ", " + Math.round(pt.latitude * 100000) / 100000 + "<br><br>"
-                        + "<b>Address: </b>" + address + "<br><br>"
-                        + "<b>Flood Zone: </b> Zone " + response.data.floodZone + "<br><br>"
-                        + "<b>Catasrophic Flood Probability: </b>" + response.data.floodZoneDesciption + "<br><br>"
-                        + "<b>Base Flood Elevation: </b>" + null + "<br><br>" //placeholder for BFE
-                        + "<b>Flood Insurance Required? </b>" + response.data.SpecialFloodHazardArea //SFA data not displaying correctly, "undefined"
-                });
-            },
-            error: function () {
-                //Swallow it, who cares lolz
-            }
-        });
-    }
+        map.add(layer); //Displays FEMA FIRM layer
  
+        view.ui.add(search, "top-right"); //Places search widget
+
+        view.ui.add(legend, "bottom-left"); //Places legend widget
+
+        Setup_Custom_Search_Response(view, search); // Fires search functions
+    });
 };
